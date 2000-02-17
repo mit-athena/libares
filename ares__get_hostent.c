107c2e793411faee5ad8634d2c353be589453d39
Greg Hudson <ghudson@mit.edu>
2000-02-17 18:39:58 +0000

    Cast hostent h_name field to char * before freeing, to pacify Linux. (This was accidentally checked in on the ares-1_0 branch before.)

--- ares__get_hostent.c
+++ ares__get_hostent.c
@@ -13,7 +13,7 @@
  * without express or implied warranty.
  */
 
-static const char rcsid[] = "$Id: ares__get_hostent.c,v 1.2 1999-10-23 19:28:13 danw Exp $";
+static const char rcsid[] = "$Id: ares__get_hostent.c,v 1.3 2000-02-17 18:39:58 ghudson Exp $";
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -145,7 +145,7 @@ int ares__get_hostent(FILE *fp, struct hostent **host)
       /* Memory allocation failure; clean up. */
       if (hostent)
 	{
-	  free(hostent->h_name);
+	  free((char *) hostent->h_name);
 	  if (hostent->h_aliases)
 	    {
 	      for (alias = hostent->h_aliases; *alias; alias++)
